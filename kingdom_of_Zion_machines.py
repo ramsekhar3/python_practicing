7807e49dce97f972ee407913a297269f9d92aa7f
ramsekhar3 <ramsekhar369551@gmail.com>
2018-11-09 12:32:21 +0530

    Update kingdom_of_Zion_machines.py

--- kingdom_of_Zion_machines.py
+++ kingdom_of_Zion_machines.py
@@ -1,5 +1,3 @@
-#!/bin/python3
-
 import math
 import os
 import random
@@ -9,38 +7,43 @@ import heapq
 from collections import defaultdict
 
 # Complete the minTime function below.
-def minTime(roads, machines):
-    n =len(roads)+1
+def minTime(roads, machines,nodes):
+    n =len(roads)
     m=len(machines)
     g=defaultdict(list)
-    roads_to_be_removed=defaultdict(list)
-    roads_vis=[[False]*n for _ in range(n)]
+    roads_vis=[[False]*nodes for _ in range(nodes)]
     for i in range(n):
         g[roads[i][0]].append((roads[i][1],roads[i][2]))
         g[roads[i][1]].append((roads[i][0],roads[i][2]))
+        #print(i,roads[i][0],roads[i][1])
         roads_vis[roads[i][0]][roads[i][1]]=True
         roads_vis[roads[i][1]][roads[i][0]]=True
-    visited=[False]*(n)
-    s=[]
+    #print (g,roads_vis)
+    roads_del=[]
+    result=0
     for temp in machines:
+        visited=[False]*(nodes)
+        s=[]
         heapq.heappush(s,temp)
+        roads_to_be_removed=list()
         while len(s)>0:
             x=heapq.heappop(s)
             visited[x]=True
             for t in g[x]:
                 k=t[0]
                 d=t[1]
-                if visited[k]==False:
+                if visited[k]==False and roads_vis[x][k] ==True:
                     visited[k]==True
-                    heapq.heappush(k)
+                    heapq.heappush(s,k)
                     roads_to_be_removed.append([x,k,d])
                     if k in machines:
-                        road_del=sorted(roads_to_be_removed,key=lambda item:item[2])
-                        roads_vis[roads_del[0][1]][roads_del[0][0]]=False
-                        roads_vis[roads_del[0][0]][roads_del[0][1]]=False
-                        result=result+roads_del[0][2]
-    
-        
+                        roads_to_be_removed=sorted(roads_to_be_removed,key=lambda item:item[2])
+                        r=roads_to_be_removed.pop(0)
+                        roads_vis[r[1]][r[0]]=False
+                        roads_vis[r[0]][r[1]]=False
+                        print (k,r)
+                        result=result+r[2]
+    return result    
     
 
 if __name__ == '__main__':
@@ -63,7 +66,7 @@ if __name__ == '__main__':
         machines_item = int(input())
         machines.append(machines_item)
 
-    result = minTime(roads, machines)
+    result = minTime(roads, machines,n)
 
     fptr.write(str(result) + '\n')
 
